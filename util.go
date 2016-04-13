b6f7bd1c696aab0cf09f09c20b7179778cc4700a
Diego Fernández Barrera <bigomby@gmail.com>
2016-04-13 17:23:21 +0200

    Moved code to have a clean main function

--- util.go
+++ util.go
@@ -9,6 +9,7 @@ import (
 	"gopkg.in/yaml.v2"
 
 	"github.com/Sirupsen/logrus"
+	"github.com/redBorder/rbforwarder"
 )
 
 // Config store the main configuration for the application
@@ -19,14 +20,66 @@ type Config struct {
 }
 
 // LoadConfigFile open an YAML file and parse the content
-func LoadConfigFile(fileName string) (config Config, err error) {
-	configData, err := ioutil.ReadFile(fileName)
+func LoadConfigFile(fileName string) (rbForwarderConfig rbforwarder.Config,
+	KafkaConfig, HTTPConfig map[string]interface{}, err error) {
+	data, err := ioutil.ReadFile(fileName)
 	if err != nil {
 		return
 	}
-
-	if err = yaml.Unmarshal([]byte(configData), &config); err != nil {
+	config := Config{}
+	if err = yaml.Unmarshal([]byte(data), &config); err != nil {
 		return
+	}
+
+	KafkaConfig = config.Kafka
+	HTTPConfig = config.HTTP
+
+	// Get number of workers
+	if workers, ok := config.Backend["workers"].(int); ok {
+		rbForwarderConfig.Workers = workers
+	} else {
+		rbForwarderConfig.Workers = defaultWorkers
+	}
+
+	// Get number of retries per message
+	if retries, ok := config.Backend["retries"].(int); ok {
+		rbForwarderConfig.Retries = retries
+	} else {
+		rbForwarderConfig.Retries = defaultRetries
+	}
+
+	// Time to wait between retries
+	if backoff, ok := config.Backend["backoff"].(int); ok {
+		rbForwarderConfig.Backoff = backoff
+	} else {
+		rbForwarderConfig.Backoff = defaultBackoff
+	}
+
+	// Get queue size
+	if queue, ok := config.Backend["queue"].(int); ok {
+		rbForwarderConfig.QueueSize = queue
+	} else {
+		rbForwarderConfig.QueueSize = defaultQueueSize
+	}
+
+	// Get max message rate
+	if maxMessages, ok := config.Backend["max_messages"].(int); ok {
+		rbForwarderConfig.MaxMessages = maxMessages
+	}
+
+	// Get max bytes rate
+	if maxBytes, ok := config.Backend["max_bytes"].(int); ok {
+		rbForwarderConfig.MaxBytes = maxBytes
+	}
+
+	// Get the interval to show message rate
+	if interval, ok := config.Backend["showcounter"].(int); ok {
+		rbForwarderConfig.ShowCounter = interval
+	}
+
+	// Show debug info
+	if *debug {
+		rbForwarderConfig.Debug = true
 	}
 
 	return
